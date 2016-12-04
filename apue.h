8ab4d03c0869127fecea47c5f215011491683e78
uuman020 <uuman020@sina.com>
2016-12-04 21:52:35 +0800

    yyz

---
+++ apue.h
@@ -0,0 +1,10 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+
+#define oops(s, n) {perror(s); exit(n);}
+#define oois(s, n) {fprintf(s); exit(n);}
+
